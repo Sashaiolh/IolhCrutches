ffd6ef3ad7061d138c06c4f44169324f1791b117
Sasha2319 <dliapromua@gmail.com>
2024-04-26 23:11:14 +0300

    fix SugarCane height

---
+++ src/main/java/org/sashaiolh/iolhcrutches/vanila/SugarCaneMixin.java
@@ -0,0 +1,28 @@
+package org.sashaiolh.iolhcrutches.vanila;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SugarCaneBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SugarCaneBlock.class)
+public class SugarCaneMixin {
+    @Inject(at = @At("HEAD"), method = "randomTick")
+    public void onRandomTick(BlockState p_222548_, ServerLevel p_222549_, BlockPos p_222550_, RandomSource p_222551_, CallbackInfo ci) {
+        int height = 0;
+        while (p_222549_.getBlockState(p_222550_).getBlock() == Blocks.SUGAR_CANE) {
+            height++;
+            if (height >= 3) {
+                ci.cancel();
+                break;
+            }
+            p_222550_ = p_222550_.below();
+        }
+    }
+}
