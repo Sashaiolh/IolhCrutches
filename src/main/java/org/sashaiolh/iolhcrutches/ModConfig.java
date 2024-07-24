4714c356a2fe2755ab50080574184b054d05d58e
Sashaiolh <149624800+Sashaiolh@users.noreply.github.com>
2024-07-24 16:56:56 +0300

    Перенос QuantumCatcherItemMixin в мод IolhCommonFixes

--- src/main/java/org/sashaiolh/iolhcrutches/ModConfig.java
+++ src/main/java/org/sashaiolh/iolhcrutches/ModConfig.java
@@ -20,8 +20,6 @@ public class ModConfig {
 
         public final ForgeConfigSpec.ConfigValue<String> test;
 
-        public final ForgeConfigSpec.ConfigValue<List<? extends String>> forbidden_arcanus_quantum_catcher_blacklist_entities;
-
         public Common(ForgeConfigSpec.Builder builder) {
             builder.push("general");
 
@@ -29,18 +27,6 @@ public class ModConfig {
                     .comment("test")
                     .define("test", "test");
 
-            builder.pop();
-
-
-            builder.push("forbidden_arcanus");
-
-            forbidden_arcanus_quantum_catcher_blacklist_entities = builder
-                    .comment("Quantum Catcher blacklist entities (regex)")
-                    .defineList("quantum_catcher_blacklist_entities",
-                            List.of("npc", "iolhbosses", "botania:doppleganger"),
-                            o -> o instanceof String);
-
-            
             builder.pop();
         }
     }
